86eee65fb1cdea068e5246fb9cb7512e497c57be
Neetagrg <neeta.kumari23@my.stjohns.edu>
2026-03-21 16:09:12 -0400

    Widen foot sep, slow step duration for better ZMP stability

--- scripts/zmp_gait_controller.py
+++ scripts/zmp_gait_controller.py
@@ -9,8 +9,8 @@ from foot               import SwingFootTrajectory, FootstepPlanner
 from inverse_kinematics import LegIKSolver
 
 DT=0.02; COM_HEIGHT=0.38; PREVIEW_HORIZON=20
-STEP_LENGTH=0.12; FOOT_SEP=0.08
-STEP_DURATION=0.6; DS_DURATION=0.1
+STEP_LENGTH=0.12; FOOT_SEP=0.12
+STEP_DURATION=0.8; DS_DURATION=0.2
 SWING_HEIGHT=0.08; N_STEPS=8
 Kp_bal=0.10; Ki_bal=0.001; Kd_bal=0.01
 
